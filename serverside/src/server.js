149d15c422c178ce2f090d0bdbd8a41cab002ee0
Vincenzo Chiarella <vins14nov@outlook.it>
2019-06-15 18:33:05 +0200

    commit first

---
+++ serverside/src/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.get('/prova', (req, res) => {
+    res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+  });
