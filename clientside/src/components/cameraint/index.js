72a21b9d54c8550bec0b6725285f4904d8da94dc
Vincenzo Chiarella <vins14nov@outlook.it>
2019-06-25 15:17:01 +0200

    camera int socketioclient

--- clientside/src/components/cameraint/index.js
+++ clientside/src/components/cameraint/index.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import socketIOClient from 'socket.io-client'
 
 
 
 class CameraInternal extends React.Component {
+    state = {
+        endpoint= " http://localhost:8080/internalcam"
+    }
+    componentDidMount(){
+        const socket = socketIOClient(this.state.endpoint)
+        socket.on("FromAPI",data => this.setState({fl}))
+    }
     render(){
         return(
         <>
-        
+
+
         </>        
         )
     }
