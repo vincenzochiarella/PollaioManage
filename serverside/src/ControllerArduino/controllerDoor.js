b7a8a7b67c55431ec952790e9d4f2a76925ef061
samucucchi <s.cucchi@outlook.com>
2019-07-02 10:19:22 +0200

    Arduino controller

--- serverside/src/ControllerArduino/controllerDoor.js
+++ serverside/src/ControllerArduino/controllerDoor.js
@@ -1,15 +1,41 @@
 var five = require("johnny-five")
-
 var board = new five.Board();
+
+board.on("ready", (motor) => {
+
+  var motor = new five.Motor({
+    pins: {
+      pwm: 3,
+      dir: 12,
+      brake: 9
+    }
+  });
+
+  motor.on("forward", function(err, timestamp) {
+    // demonstrate braking after 5 seconds
+    board.wait(5000, function() {
+      motor.brake();
+    });
+  });
+
+  motor.on("brake", function(err, timestamp) {
+    // Release the brake after .1 seconds
+    board.wait(100, function() {
+      motor.stop();
+    });
+  });
+
+  // Start the motor at maximum speed
+  motor.forward(255);
+
+});
+
+/*
 var led = null;
 board.on("ready", (led) =>{
   led = new five.Led(13)
-  // led.blink(1000)
+  led.off()
 })
-
-
-
-
-
+*/
 
 module.exports = led
