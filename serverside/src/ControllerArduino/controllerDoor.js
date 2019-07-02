3e5a90026992fd709719e8c5caf7622e30221d29
samucucchi <s.cucchi@outlook.com>
2019-07-02 10:59:48 +0200

    Motor moving

--- serverside/src/ControllerArduino/controllerDoor.js
+++ serverside/src/ControllerArduino/controllerDoor.js
@@ -1,7 +1,7 @@
-var five = require("johnny-five")
-var board = new five.Board();
+var five = require("johnny-five"),
+  board = new five.Board();
 
-board.on("ready", (motor) => {
+board.on("ready", function() {
 
   var motor = new five.Motor({
     pins: {
@@ -11,31 +11,44 @@ board.on("ready", (motor) => {
     }
   });
 
-  motor.on("forward", function(err, timestamp) {
+  board.repl.inject({
+    motor: motor
+  });
+
+  motor.on("start", function() {
+    console.log("start", Date.now());
+  });
+
+  motor.on("stop", function() {
+    console.log("automated stop on timer", Date.now());
+  });
+
+  motor.on("brake", function() {
+    console.log("automated brake on timer", Date.now());
+  });
+
+  motor.on("forward", function() {
+    console.log("forward", Date.now());
+
+    // demonstrate switching to reverse after 5 seconds
+    board.wait(5000, function() {
+      motor.reverse(255);
+    });
+  });
+
+  motor.on("reverse", function() {
+    console.log("reverse", Date.now());
+
     // demonstrate braking after 5 seconds
     board.wait(5000, function() {
-      motor.brake();
+
+      // Brake for 500ms and call stop()
+      motor.brake(500);
     });
   });
 
-  motor.on("brake", function(err, timestamp) {
-    // Release the brake after .1 seconds
-    board.wait(100, function() {
-      motor.stop();
-    });
-  });
-
-  // Start the motor at maximum speed
+  // set the motor going forward full speed
   motor.forward(255);
-
 });
 
-/*
-var led = null;
-board.on("ready", (led) =>{
-  led = new five.Led(13)
-  led.off()
-})
-*/
-
-module.exports = led
+//module.exports = led
