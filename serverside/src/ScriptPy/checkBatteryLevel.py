db45934e189891aba2ce1ce8ec26e4bb7640f800
samucucchi <s.cucchi@outlook.com>
2019-07-25 17:40:28 +0200

    Battery level indicator

---
+++ serverside/src/ScriptPy/checkBatteryLevel.py
@@ -0,0 +1,16 @@
+from nanpy import (ArduinoApi, SerialManager)
+from time import sleep
+
+batteryPin = 'A0' 
+connection = SerialManager('COM5')
+a = ArduinoApi(connection = connection)
+eps = 0.0048828125
+
+#Setup 
+a.pinMode(batteryPin, a.INPUT)
+
+#Loop
+batteryLevel = eps * a.analogRead(batteryPin)
+converted = (batteryLevel * 12) / 5
+percentage = (converted * 100) / 12
+print(int(percentage))
