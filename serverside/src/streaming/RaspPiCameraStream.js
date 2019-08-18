9ee79352596fea026a3530f85e7d398664f5c318
Vincenzo Chiarella <vins14nov@outlook.it>
2019-08-18 18:32:07 +0200

    fix 6

--- serverside/src/streaming/RaspPiCameraStream.js
+++ serverside/src/streaming/RaspPiCameraStream.js
@@ -2,7 +2,7 @@ const RstpStreamVlc = require('child_process')
 
 var process = null
 module.exports.startVlcRTSP = function startScript(){
-    process = RstpStreamVlc.spawn('raspivid',['-o','-','-t','0','-n','-w','1280','-h','720','-fps','15','|', 'cvlc', '-vvv', 'stream:///dev/stdin', '--sout', '"' + '#rtp{sdp=rtsp://:8554/}'+ '"', ':demux=h264]'])
+    process = RstpStreamVlc.exec(`raspivid -o - -t 0 -n -w 1280 -h 720 -fps 15 | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/}' :demux=h264`)
     console.log('Start process ' +process.pid)
 }
 
