f6f8d2ad7550ac1c5c105f978743fb4ee8ca196e
Vincenzo Chiarella <vins14nov@outlook.it>
2019-08-26 16:03:44 +0200

    return test 17

--- serverside/src/streaming/RaspPiCameraStream.js
+++ serverside/src/streaming/RaspPiCameraStream.js
@@ -1,18 +1,14 @@
 const RstpStreamVlc = require('child_process')
 
-var process = []
+var process = null
 module.exports.startVlcRTSP = function startScript(){
     process = RstpStreamVlc.exec(`raspivid -o - -t 0 -n -w 858 -h 480 -fps 15 | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/}' :demux=h264`)
-    process.map( pro => {
         console.log(pro.pid)
-    })
 }
 
 module.exports.stopVlcRTSP = function stopScript(){
     console.log('Process killed '+ process.pid)
-    process.map( pro =>{
-        pro.kill()
-    })
+    process.kill()
 }
 
 
