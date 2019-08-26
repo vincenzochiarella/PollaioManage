0a380e0d836738dff2d262f0769bd9c7a58415d2
Vincenzo Chiarella <vins14nov@outlook.it>
2019-08-26 15:59:06 +0200

    test 16 low latency

--- serverside/src/streaming/RaspPiCameraStream.js
+++ serverside/src/streaming/RaspPiCameraStream.js
@@ -2,7 +2,7 @@ const RstpStreamVlc = require('child_process')
 
 var process = []
 module.exports.startVlcRTSP = function startScript(){
-    process = RstpStreamVlc.exec(`raspivid -o - -t 0 -n -w 1280 -h 720 -fps 15 | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/}' :demux=h264`)
+    process = RstpStreamVlc.exec(`raspivid -o - -t 0 -n -w 858 -h 480 -fps 15 | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/}' :demux=h264`)
     process.map( pro => {
         console.log(pro.pid)
     })
