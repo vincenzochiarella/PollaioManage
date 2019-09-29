aae4adc8f46d37976f313ac978075cd9f55f2391
Vincenzo Chiarella <vins14nov@outlook.it>
2019-09-29 19:50:52 +0200

    fix index scripts

--- serverside/src/ScriptPy/index.js
+++ serverside/src/ScriptPy/index.js
@@ -18,7 +18,7 @@ module.exports.runOpendoor = pyOpen = () => {
         wasBrightActive = true
     }
     if (lockProcess === null) {
-        lockProcess = spawn('python', [pathFolder + '/testProcess.py'])
+        lockProcess = spawn('python', [pathFolder + '/OpenDoor.py'])
         console.log('Spawned open: ' + lockProcess.pid)
         lockProcess.on('close', () => {
             if (wasBrightActive)
@@ -37,7 +37,7 @@ module.exports.runClosedoor = pyClose = () => {
         wasBrightActive = true
     }
     if (lockProcess === null) {
-        lockProcess = spawn('python', [pathFolder + '/testProcess.py'])
+        lockProcess = spawn('python', [pathFolder + '/CloseDoor.py'])
         console.log('Spawned close: ' + lockProcess.pid)
         lockProcess.on('close', () => {
             if (wasBrightActive)
@@ -48,25 +48,29 @@ module.exports.runClosedoor = pyClose = () => {
 
 module.exports.startBatteryLevelTakeOver = pyBatteryLevel = () => {
     console.log("Ricava livello batteria")
-    // if (lockProcess === null) {
-    //     var wasBrightActive = false
-    //     if (BrightnessTakeOver_Process){
-    //         stopBright()
-    //         wasBrightActive = true
-    //     }
-    //     BatteryLevel_Process = spawn('python', [pathFolder + '/testProcess.py'])
-    //     BatteryLevel_Process.stdout.on('data', (data) => {
-    //         BatteryLevel.create({
-    //             value: data
-    //         }).then(() => { }).catch((err) => { console.log(err) })
-    //         BatteryLevel_Process.exit(0)
-    //     })
-    //     if(wasBrightActive)
-    //         pyBright()
-    // }
+    if (lockProcess === null) {
+        let wasBrightActive = false
+        if (BrightnessTakeOver_Process){
+            stopScriptBrightness()
+            wasBrightActive = true
+        }
+        try{
+            BatteryLevel_Process = spawn('python', [pathFolder + '/checkBatteryLevel.py'])
+            BatteryLevel_Process.stdout.on('data', (data) => {
+                BatteryLevel.create({
+                    value: data
+                }).then(() => { }).catch((err) => { console.log(err) })
+
+            })
+            process.kill(BatteryLevel_Process.pid)
+            setTimeout(() => runScriptBrightness(), 2000)
+        }catch(err){}
+        if(wasBrightActive)
+            pyBright()
+    }
 }
 function runScriptBrightness() {
-    BrightnessTakeOver_Process = spawn('python', [pathFolder + '/testProcess.py'])
+    BrightnessTakeOver_Process = spawn('python', [pathFolder + '/GetBrightness.py'])
     console.log(BrightnessTakeOver_Process.pid)
     BrightnessTakeOver_Process.stdout.on('data', (data) => {
         Brightness.create({
